3d82114718ff538304581ec09ba0b94352868159
Gianni <sb5021087@gmail.com>
2024-03-19 11:59:26 -0500

    feat: Add Terraform code for Azure VM deployment

---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "name_vm" {
+  type        = string
+  description = "Name Function"
+}
+
+variable "location" {
+  type        = string
+  default     = "eastus"  
+  description = "Location"
+}
